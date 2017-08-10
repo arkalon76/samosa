4c10a31a7a8d11c5946c6a2d606a12eef740bda8
Kenth Fagerlund <kenth@cherrypicks.com>
2017-08-10 14:37:19 +0800

    Added IMDB lookup /w tests

--- samosa/__init__.py
+++ samosa/__init__.py
@@ -1,6 +1,10 @@
 import os
+import guessit
+from imdbpie import Imdb
+from guessit import guessit
 
 SUPPORTED_CONTAINERS = ['.mkv', '.avi', '.mp4', '.m4v']
+imdb = Imdb(anonymize=True)  # to proxy requests
 
 
 def find_all_media_files(dir_to_search, extentions=SUPPORTED_CONTAINERS):
@@ -10,3 +14,23 @@ def find_all_media_files(dir_to_search, extentions=SUPPORTED_CONTAINERS):
             if os.path.splitext(filename)[1] in extentions:
                 file_list.append(filename)
     return file_list
+
+
+def resolve_imdb_from_filename(test_file):
+    # First, let's extract the name of the movie and it's year
+    nameDict = guessit(test_file)
+    try:
+        title = nameDict['title']
+        year = str(nameDict['year'])
+    except KeyError:
+        print('This file "' + test_file + '" seems oddly named.\
+              Please follow [title] [year] format')
+        return None
+    imdbResult = imdb.search_for_title(title)
+
+    print('\n')
+    print(imdbResult)
+    for movie in imdbResult:
+        if year == movie['year']:
+            print('Match found')
+            return movie['imdb_id']
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,11 +2,19 @@ import pytest
 import samosa
 
 
-# @pytest.mark.parametrize("test_file", [
-#     ('Kimi.no.na.wa.aka.Your.Name.2016.JAPANESE.1080p.BluRay.REMUX.AVC.DTS-HD.MA.5.1-FGT.mkv'),
-#     ('Looney.Tunes.Volume.2.1936-1959.1080p.BluRay.REMUX.AVC.DD1.0-RARBG.mkv'),
-#     ('The.Princess.And.The.Frog.2009.1080p.BluRay.AVC.DTS-HD.MA.5.1-FGT.avi'),
-# ])
+@pytest.mark.parametrize("test_file,expected", [
+    ('Ghost.In.The.Shell.2017.1080p.3D.BluRay.AVC.TrueHD.7.1.Atmos-FGT.mkv', 'tt1219827'),
+    ('The.Princess.And.The.Frog.2009.1080p.BluRay.AVC.DTS-HD.MA.5.1-FGT.mkv', 'tt0780521'),
+    ('Shot.Caller.2017.1080p.BluRay.REMUX.AVC.TrueHD.5.1-FGT.mkv', 'tt4633690'),
+    ('Random.Bullshit.without.year.mkv', None),
+    ('.git.filename', None)
+])
+
+
+def test_resolve_imdb_id_from_filename(test_file, expected):
+    imdb_id = samosa.resolve_imdb_from_filename(test_file)
+    assert imdb_id == expected
+
 
 @pytest.fixture
 def movielist():
